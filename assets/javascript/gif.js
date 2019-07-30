caacdf99843f7d6eeb15a2edf66e5e9ef297523d
Leonard Ezeudu <leonardezeudu@gmail.com>
2019-07-30 15:23:13 -0400

    added more css properties

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,68 +1,23 @@
-
-var gifList = [];
+// Event handler On-click function for the search button
 
 $("#search").on("click", function() {
-
-    var gif = $("#input").val();
-    getGif(gif);
+    $("#gifs").html("");
+    $("#input").focus();
+    var userSearch = $("#input").val();
+    console.log(userSearch);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=83Fgh3I1oIoXKbpTYDtScko02CRuyMxT&limit=5";
     
-  });
+    console.log(queryURL);
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		for (var i = 0; i < 10; i++) {
+			console.log(response.data[i]);
+			$("#gifs").append("<img alt='" + response.data[i].type + "' src='" + response.data[i].images.fixed_height_downsampled.url + "'>");
+		}
+    });
+    
+    $("#input").val() = "";
 
-
-
-
-function gifElement(response) {
-    var gifSection = $("<section>");
-    gifSection.addClass("card");
-
-    var gifImage = $("<img>");
-    gifImage.attr("src", response.Poster);
-    gifImage.attr("alt", response.Title+" Poster");
-
-    var gifPoster = $("<figure>");
-    gifPoster.append(gifImage);
-    gifSection.append(gifPoster);
-
-    var gifName = $("<h1>");
-        gifName.addClass("card-title");
-      gifName.html(response.Title);
-      gifSection.append(gifName);
-
-      var gifDescription = $("<p>");
-      gifDescription.addClass("card-text");
-      gifDescription.html(response.Plot);
-      gifSection.append(gifDescription);
-
-      $("body").append(gifSection);
-
-} 
-
-function getGif(gif) {
-
-
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=83Fgh3I1oIoXKbpTYDtScko02CRuyMxT&limit=5"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-      console.log(response)
-    // if (response.Response === "False") {
-    //     alert(response.Error);
-    //   }
-    //   else if (gifList.indexOf(response.Title) >= 0) {
-    //     alert ("Gif already in List!");
-    //   }
-    //   else {
-    //     gifElement(response);
-    //     gifList.push(response.Title);
-    //   }
-  });
-}
-
-for (var i = 0; i < gifList.length; i++) {
-
-    var gif = gifList[i];
-    gifElement(gif);
-
-  }
+});
